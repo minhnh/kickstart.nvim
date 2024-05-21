f74ffac825d92a081507999af408aac97917a40d
Minh Nguyen <1168534+minhnh@users.noreply.github.com>
2024-05-21 08:58:02 +0200

    config tab

--- init.lua
+++ init.lua
@@ -147,6 +147,13 @@ vim.opt.splitbelow = true
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
+-- tabs
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
 
