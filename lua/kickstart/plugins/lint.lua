5b7e6fcd2bd86a0bb8633b96e6edac9b39394cd8
Minh Nguyen <1168534+minhnh@users.noreply.github.com>
2024-06-04 00:55:57 +0200

    configure languages, lint, replace keymap

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,8 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        python = { 'ruff' },
+        cpp = { 'clangtidy' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
