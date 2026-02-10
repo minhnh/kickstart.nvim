e1fb964c5578b47cdd364ce1a3211ab98aafee65
Minh Nguyen <1168534+minhnh@users.noreply.github.com>
2026-02-10 16:00:50 +0100

    add replace keymap

--- init.lua
+++ init.lua
@@ -225,6 +225,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- User defined keymaps
+vim.keymap.set('n', '<F3>', [[ ':%s/'.expand('<cword>').'//cgI' ]], { expr = true, noremap = true, desc = 'Search and replace current word in file' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
